3fb5879a9146a1c5b6aff99303805ebff633848e
sebmanogithub <sebastiengithub@gmail.com>
2025-01-31 09:16:00 +0100

    Création de l'Api de recup des users - UsersCOntroller

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec5f7a783b7855bd03a00ff0403e9e89e75a7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac2f17241f550e1fede883e903c8e917dbe88ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
