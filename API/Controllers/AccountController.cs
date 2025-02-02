b52e110c841093732fbcc1d82920eb58f2d4eeed
sebmanogithub <sebastiengithub@gmail.com>
2025-02-02 17:03:29 +0100

    Création de AccountController et de la méthode d'enregistrement register du user HttpPost

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDtos registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+            {
+                return BadRequest("UserName is taken");
+            }
+
+            using var hmac = new HMACSHA512();
+            AppUser user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private async Task<bool> UserExists(string UserName)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == UserName.ToLower());
+        }
+    }
+}
