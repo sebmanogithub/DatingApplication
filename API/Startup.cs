1d843df769899368d8c8795a202385f3a8fecff6
sebmanogithub <sebastiengithub@gmail.com>
2025-02-02 15:13:50 +0100

    Installation Certificat de sécurité SSL

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,7 +56,7 @@ namespace API
             app.UseCors(policy => policy
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .WithOrigins("http://localhost:4200"));
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
