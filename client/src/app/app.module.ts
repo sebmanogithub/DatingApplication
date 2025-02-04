ad975e1eefc200242567de424715ffe201721915
sebmanogithub <sebastiengithub@gmail.com>
2025-02-04 15:28:33 +0100

    Création du composant barre de navigation et implémentation du premier service accountService (login pour commencer

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { Nav2Component } from './nav2/nav2.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    Nav2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
