767df65cace5cd05d3dfc3c585e50b9a5fc693ee
sebmanogithub <sebastiengithub@gmail.com>
2025-02-05 09:23:42 +0100

    Utilisation de l'async | dans le fichier html afin de souscrire à l'abservable directement

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-nav',
@@ -8,34 +10,22 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean = false
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error)
     })
   }
 
   logout() {
+    console.log('NavComponent LOGOUT');
     this.accountService.logout();
-    this.loggedIn = false
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      // !! turning object into boolean
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    })
   }
 }
