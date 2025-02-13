b874e4624e9e2c961e9176fd06dfbe85b5f3bb86
sebmanogithub <sebastiengithub@gmail.com>
2025-02-13 08:42:43 +0100

    Ajout des composants Matches, Lists, Messages et gestion du routage

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35505f1fb679edec5ed224d4f1faaaa258bf92f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93384cc568032494ee082c090149b8340c1351ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
