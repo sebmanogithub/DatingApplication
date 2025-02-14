bd7192f6928759792e65689b2f19eb153a20369d
sebmanogithub <sebastiengithub@gmail.com>
2025-02-14 14:38:44 +0100

    Enrichissement de l'entité AppUser et création de l'entité Photo

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78a9ddea2ed1637c5d44da566030218cc72670c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a78830be7c4c1028280d39128ab2026d3234890")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
