4907937b0bb8e9e07b3541c2e22dc97799612cee
sebmanogithub <sebastiengithub@gmail.com>
2025-02-15 16:07:33 +0100

    Implementation du composant memberDetail

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,16 +1,10 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/members';
 
 const user = localStorage.getItem('user');
 const token = user ? JSON.parse(user)?.token : null;
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: token ? `Bearer ${token}` : ''
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -21,14 +15,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users',
-      httpOptions
-    )
+    return this.http.get<Member[]>(this.baseUrl + 'users')
   }
 
   getMember(username : string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username,
-      httpOptions
-    )
+    return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MembersService } from 'src/app/_services/members.service';
+import { Member } from 'src/app/models/members';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member?: Member
 
-  constructor() { }
+  constructor(private memberService: MembersService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
+  loadMembers() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')!)
+    .subscribe(member => {
+      this.member = member
+    })
+  }
 }
