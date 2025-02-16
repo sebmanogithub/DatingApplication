ee74bbc55a5d6332b77c2ef62d066bcb2bf835b6
sebmanogithub <sebastiengithub@gmail.com>
2025-02-16 13:56:14 +0100

    Sauvegarde de l'état des membres dans le service member afin de ne plus requêter en base si non nécessaire -> Persistance des données

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { MembersService } from 'src/app/_services/members.service';
 import { Member } from 'src/app/models/members';
 
@@ -8,18 +9,12 @@ import { Member } from 'src/app/models/members';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members: Member[] = [];
+  members$: Observable<Member[]> = new Observable<Member[]>();
 
   constructor(private memberService : MembersService) { }
 
   ngOnInit(): void {
-    console.log("init");
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
   }
 
-  loadMembers() {
-    this.memberService.getMembers().subscribe(members => {
-      this.members = members;
-    });
-  }
 }
