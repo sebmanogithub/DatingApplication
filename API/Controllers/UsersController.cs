616abeecdf4595c3867fb7e361c7c0e3d644fadc
sebmanogithub <sebastiengithub@gmail.com>
2025-02-17 14:24:08 +0100

    ajout du package ng2-file-upload afin de pouvoir charger de nouvelles photos dans son profil

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -94,5 +94,33 @@ namespace API.Controllers
 
             return BadRequest("Problem addding photo");
         }
+
+        // [HttpPut("set-main-photo/{photoId}")]
+        // public async Task<ActionResult> SetMainPhoto(int photoId)
+        // {
+        //     var user = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
+
+        //     var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+
+        //     if (photo.IsMain)
+        //     {
+        //         return BadRequest("This is already your main photo");
+        //     }
+
+        //     var currentMainPhoto = user.Photos.FirstOrDefault(x => x.IsMain);
+        //     if (currentMainPhoto != null)
+        //     {
+        //         currentMainPhoto.IsMain = false;
+        //     }
+
+        //     photo.IsMain = true;
+
+        //     if (await _userRepository.SaveAllAsync())
+        //     {
+        //         return NoContent();
+        //     }
+
+        //     return BadRequest("Failed to set main photo");
+        // }
     }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
+import { AccountService } from 'src/app/_services/account.service';
 import { Member } from 'src/app/models/members';
+import { User } from 'src/app/models/user';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-photo-editor',
@@ -8,9 +13,45 @@ import { Member } from 'src/app/models/members';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() member?: Member;
-  constructor() { }
+  uploader: FileUploader = new FileUploader({});
+  hasBaseDropzoneOver = false;
+  baseUrl = environment.apiUrl;
+  user?: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).
+      subscribe(user => this.user = user);
+   }
 
   ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(event: any){
+    this.hasBaseDropzoneOver = event;
+  }
+
+  initializeUploader() {
+    this.uploader.setOptions({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user?.token,
+      isHTML5 : true,
+      allowedFileType: ['image'],
+      removeAfterUpload: false,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    })
+
+    this.uploader.onAfterAddingAll = (file) => {
+      file.withCredentials = false
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member?.photos.push(photo);
+      }
+    }
   }
 
 }
