a9331f379bdbd649b5e26774b28bca57b1ec96dc
sebmanogithub <sebastiengithub@gmail.com>
2025-02-18 09:40:43 +0100

    Mise en place des contrôles et validations pour l'enregistrement d'un user - composant register avec l'implémentation des Reactives Forms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +10,39 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {}
+  model: any = {};
+  registerForm!: FormGroup;
 
   constructor(
     private accountService : AccountService,
     private toaster : ToastrService) { }
 
   ngOnInit(): void {
+    this.iniatilizeForm();
+  }
+
+  iniatilizeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      password: new FormControl('', [Validators.required, 
+                        Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    })
+
+    this.registerForm.controls.password?.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword?.updateValueAndValidity();
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      const parent = control.parent as FormGroup;
+      if (!parent) return null; // Sécurité pour éviter des erreurs si `parent` est null
+  
+      return control.value === parent.controls[matchTo]?.value
+        ? null 
+        : { isMatching: true };
+    };
   }
 
   register(){
