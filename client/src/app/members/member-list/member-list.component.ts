c55b3282d52babcc784268a2064312a4548ada0f
sebmanogithub <sebastiengithub@gmail.com>
2025-02-19 10:01:42 +0100

    Implémentation de la pagination côté client

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MembersService } from 'src/app/_services/members.service';
 import { Member } from 'src/app/models/members';
+import { Pagination } from 'src/app/models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -9,11 +10,22 @@ import { Member } from 'src/app/models/members';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]> = new Observable<Member[]>();
+  members: Member[] = [];
+  pagination!: Pagination;
+  pageNumber = 1;
+  pageSize = 6;
 
   constructor(private memberService : MembersService) { }
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize)
+      .subscribe(response => {
+        this.members = response.result!;
+        this.pagination = response.pagination;
+    })
   }
 }
