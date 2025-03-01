3e2d848da955fd767e2ef52fe3e4a76e35eda948
sebmanogithub <sebastiengithub@gmail.com>
2025-03-01 13:53:36 +0100

    Création de politiques d'autorisation - nouveau controlleur AdminCOntrolleur

--- API/obj/Debug/net5.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net5.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef7f4f272d2d23364b0063f75494b14cb451cf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9fc187bfd4d56b0e33e49c36ed2f0a9c3dbf7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
